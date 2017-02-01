d48828262942be1e3cc28b1e2d78f879a01365d6
OOuissem <smartmen0909@gmail.com>
2017-02-01 17:25:23 +0100

    internationalization in application_controller.rb , en.yml   and fr.yml

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :set_i18n_locale 
+  I18n.available_locales = [:fr, :de, :es, :en]
+  I18n.default_locale = :fr
+
+  def set_i18n_locale    
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
 end
